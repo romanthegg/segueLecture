fc60509707190163540e7238c87a948a3348d63d
romanthegg <rzadorskyy3454@stu.d214.org>
2021-01-25 22:55:09 +0200

    Initial Commit

---
+++ segueLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segueLecture
+//
+//  Created by Yanik Kukulnik on 25.01.2021.
+//  Copyright © 2021 Yanik Kukulnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
